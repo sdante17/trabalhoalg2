ca8a09d80154562b1ad5230e49fe329b8cd7dd0c
Dante de Souza <101759677+sdante17@users.noreply.github.com>
2024-09-16 14:26:04 -0300

    disciplinas.c

--- disciplinas.c
+++ disciplinas.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_PREREQUISITOS 16
-#define MAX_DISCIPLINAS 32
+#define MAX_PREREQUISITOS 10
+#define MAX_DISCIPLINAS 50
+#define MAX_SEMESTRES 10
 
 void escreveTopo(){
-    printf("_____________________________________________________________________________________________________________________________________________\n                                                               Bem-vindo ao Vasco!\n_____________________________________________________________________________________________________________________________________________");
+    printf("_____________________________________________________________________________________________________________________________________________\n                                                               Bem-vindo ao Vasco!\n_____________________________________________________________________________________________________________________________________________\n");
     
 }
-
 
 // Definição do enum para o tipo de disciplina
 typedef enum {
@@ -39,7 +39,29 @@ int main(){
 
     escreveTopo();
 
-    lmatriz();
+    FILE *file;
+
+    file = fopen("disciplinas.bin", "rb");
+    //contando a quantidade de registros no arquivo
+    fseek(file, 0, SEEK_END); //mover o ponteiro para o final
+
+    long tamanhoArquivo = ftell(file); //declara o tamanho total do arquivo em questão
+
+    rewind(file); //rebobinar, mandar ponteiro para o ínicio novamente
+
+    int quantidadeRegistros = tamanhoArquivo / sizeof(Disciplina); //calculando a quatidade de registros
+
+    Disciplina disciplinas[quantidadeRegistros]; //array para armazenar os dados
+
+    //fazendo a leitura dos dados
+    fread(disciplinas, sizeof(Disciplina), quantidadeRegistros, file); //onde armazenar, tamanho da informação, tamanho da nossa leitura do arquivo, fonte
+
+    fclose(file);
+
+    //varrer o vetor
+    for(int i = 0; i < quantidadeRegistros; i++){
+        printf("", ); //printando os dados armazenados no array "disciplinas"
+    }
 
     return 0;
 }
@@ -47,7 +69,7 @@ int main(){
 void lmatriz(){
     FILE *file;
 
-    file = fopen("disciplinas.zip", "r");
+    file = fopen("disciplinas.bin", "r");
 
     Disciplina disciplinas[MAX_DISCIPLINAS];
 
